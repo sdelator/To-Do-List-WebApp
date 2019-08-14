e8496598d896094b25f82338a00dd6c8aa64e4fd
Sandra <tiredwakoo@gmail.com>
2019-08-14 11:03:10 -0700

    adding all current files to Github Repo

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+//express routes here
+app.get("/", function (req, res) {
+    res.render("index.ejs");
+});
+
+
+
+//server listening on port 3000
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+})
