1282c8abe5b58cd02e5b588afe4bf16dd8476aeb
Sandra <tiredwakoo@gmail.com>
2019-08-15 11:27:33 -0700

    added submit button functionality

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+var bodyParse = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParse.urlencoded({extended:true}));
 
 //array of things to do
-const toDoList = [
+var toDoList = [
     "wash the car",
     "do laundry",
     "make dinner"
@@ -13,11 +15,23 @@ const toDoList = [
 
 
 //express routes here
+//default route
 app.get("/", function (req, res) {
-    res.render("index.ejs");
+    res.render("index.ejs", {toDoList: toDoList});
 });
 
+//submit button route
+app.post("/newtodo", function(req, res){
+    console.log("item submitted!");
+    var item = req.body.item;
+    toDoList.push(item);
+    res.redirect("/");
+});
 
+//catch all other routes
+app.get("*", function(req, res){
+    res.send("<h1>Invalid Page</h1>")
+});
 
 //server listening on port 3000
 app.listen(3000, function(){
