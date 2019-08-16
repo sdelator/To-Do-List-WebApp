fd25041f8a961a818d483231daf304396aedad33
Sandra <tiredwakoo@gmail.com>
2019-08-16 12:30:29 -0700

    now routing todo list to database in mongodb

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
 const express = require("express");
 const app = express();
 var bodyParse = require("body-parser");
+var mongoose = require("mongoose");
+//mongoose connection
+mongoose.connect("mongodb://localhost/todo");
 
 app.set("view engine", "ejs");
 app.use(bodyParse.urlencoded({extended:true}));
 
-//array of things to do
-var toDoList = [
-    "wash the car",
-    "do laundry",
-    "make dinner"
-]
+//mongoose schema 
+var todoSchema = new mongoose.Schema({
+    name: String
+})
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+// //array of things to do
+// var toDoList = [
+//     "wash the car",
+//     "do laundry",
+//     "make dinner"
+// ]
 
 
 
 //express routes here
 //default route
 app.get("/", function (req, res) {
-    res.render("index.ejs", {toDoList: toDoList});
+    Todo.find({}, function(err, toDoList){
+        if(err) console.log(err);
+        else{
+            res.render("index.ejs", {toDoList: toDoList});
+        }
+    })
+
 });
 
 //submit button route
 app.post("/newtodo", function(req, res){
     console.log("item submitted!");
-    var item = req.body.item;
-    toDoList.push(item);
+    var newItem = new Todo({
+        name: req.body.item
+    });
+    Todo.create(newItem, function(err, Todo){
+        if(err) console.log(err);
+        else{
+            console.log("Inserted item: " + newItem)
+        }
+    })
     res.redirect("/");
 });
 
